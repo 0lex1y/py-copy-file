be10da079809ea65dc0bb2817e9a8aca29aef790
0lex1y <kotov.oleksiy@icloud.com>
2025-09-17 14:23:31 +0300

    Fix r/w

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ def copy_file(command: str) -> None:
     if source_file_name == destination_file_name:
         return
     try:
-        with (open(source_file_name, "rb") as file_in,
-              open(destination_file_name, "wb") as file_out):
+        with (open(source_file_name, "r") as file_in,
+              open(destination_file_name, "w") as file_out):
             content = file_in.read()
             file_out.write(content)
     except FileNotFoundError:
